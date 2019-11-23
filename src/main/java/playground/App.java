a6204784c76e033d7510a12c2cde4c54732d1333
Clique Admin <cliquedmn@gmail.com>
2019-11-23 00:17:56 -0800

    Add checkstyle to the project

--- src/main/java/playground/App.java
+++ src/main/java/playground/App.java
@@ -4,11 +4,11 @@
 package playground;
 
 public class App {
-    public String getGreeting() {
+    public final String getGreeting() {
         return "Hello world.";
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(new App().getGreeting());
     }
 }

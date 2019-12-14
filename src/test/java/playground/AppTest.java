3f64f7ae61fc5c59b6b77828a740a3f85083dd66
asim516 <57764162+asim516@users.noreply.github.com>
2019-12-14 17:07:48 +0530

    updated code

--- src/test/java/playground/AppTest.java
+++ src/test/java/playground/AppTest.java
@@ -1,15 +1,65 @@
-package playground;
+  package playground;
 
-import static org.junit.Assert.assertNotNull;
+class Shapes
+{
+  public static void main(String arg[])
+  {
+    Rectangle r = new Rectangle(10, 4);
+    Square s = new Square(7);
+    Circle c = new Circle(3.5);
 
-import org.junit.Test;
+    System.out.println("Rectangle Area : " + r.getArea());
+    System.out.println("Square Area : " + s.getArea());
+    System.out.println("Circle Area : " + c.getArea());
+    System.out.println();
 
-public class AppTest {
 
-  @Test
-  public void testAppHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull("app should have a greeting",
-        classUnderTest.getGreeting());
+  }
+}
+
+class Rectangle
+{
+  double length;
+  double breadth;
+
+  Rectangle(double length, double breadth)
+  {
+    this.length = length;
+    this.breadth = breadth;
+  }
+
+  double getArea()
+  {
+    return length * breadth;
+  }
+}
+
+class Square
+{
+  double side;
+
+  Square(double side)
+  {
+    this.side = side;
+  }
+
+  double getArea()
+  {
+    return side * side;
+  }
+}
+
+class Circle
+{
+  double radius;
+
+  Circle(double radius)
+  {
+    this.radius = radius;
+  }
+
+  double getArea()
+  {
+    return (22.0/7.0) * radius * radius;
   }
 }
